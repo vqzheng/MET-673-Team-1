0a13e6a78f6aafcaa900e9c2084a7d60d382a8eb
Lei Yang <lei.yang.cs.art@gmail.com>
2017-11-06 16:19:50 -0500

    Update project setting to enable second tank shot

--- TankTest/Assets/Scripts/Tank/TankShooting.cs
+++ TankTest/Assets/Scripts/Tank/TankShooting.cs
@@ -8,7 +8,7 @@ namespace Complete
         public int m_PlayerNumber = 1;              // Used to identify the different players.
         public Rigidbody m_Shell;                   // Prefab of the shell.
         public Transform m_FireTransform;           // A child of the tank where the shells are spawned.
-        public Slider m_AimSlider;                  // A child of the tank that displays the current launch force.
+        // public Slider m_AimSlider;                  // A child of the tank that displays the current launch force.
         public AudioSource m_ShootingAudio;         // Reference to the audio source used to play the shooting audio. NB: different to the movement audio source.
         public AudioClip m_ChargingClip;            // Audio that plays when each shot is charging up.
         public AudioClip m_FireClip;                // Audio that plays when each shot is fired.
@@ -27,7 +27,7 @@ namespace Complete
         {
             // When the tank is turned on, reset the launch force and the UI
             m_CurrentLaunchForce = m_MinLaunchForce;
-            m_AimSlider.value = m_MinLaunchForce;
+            // m_AimSlider.value = m_MinLaunchForce;
         }
 
 
@@ -44,7 +44,7 @@ namespace Complete
         private void Update ()
         {
             // The slider should have a default value of the minimum launch force.
-            m_AimSlider.value = m_MinLaunchForce;
+            // m_AimSlider.value = m_MinLaunchForce;
 
             // If the max force has been exceeded and the shell hasn't yet been launched...
             if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
@@ -70,7 +70,7 @@ namespace Complete
                 // Increment the launch force and update the slider.
                 m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
 
-                m_AimSlider.value = m_CurrentLaunchForce;
+                // m_AimSlider.value = m_CurrentLaunchForce;
             }
             // Otherwise, if the fire button is released and the shell hasn't been launched yet...
             else if (Input.GetButtonUp (m_FireButton) && !m_Fired)
