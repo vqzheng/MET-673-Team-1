bd3b3a3ba6d87d0ccb2f22da304c1dc5afbfc26b
wht931011 <wht931011@gmail.com>
2017-12-11 00:21:09 -0500

    Add pause menu

---
+++ TankTest/Assets/Scripts/Manager/PauseManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour {
+	public GameObject pauseMenu;
+	public bool pause;
+
+	// Use this for initialization
+	void Start () {
+		pause = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			pause = !pause;
+		}
+
+		if (pause) {
+			pauseMenu.SetActive(true);
+			Time.timeScale = 0;
+		} else {
+			pauseMenu.SetActive(false);
+			Time.timeScale = 1;
+		}
+	}
+
+	public void LoadGame () {
+		SceneManager.LoadScene(1);
+	}
+
+	public void QuitGame () {
+		SceneManager.LoadScene(0);
+	}
+
+	public void ResumeGame () {
+		pause = false;
+		pauseMenu.SetActive(false);
+		Time.timeScale = 1;
+	}
+}
