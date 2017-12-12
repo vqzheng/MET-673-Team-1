656a2ecc2ea3876a077dae78af1f856eaaa0324d
wht931011 <wht931011@gmail.com>
2017-12-12 00:07:47 -0500

    Fix shell explosion bug. Add collider to all prefabs in city map. Make the button in main menu can load new map on click.

--- TankTest/Assets/Scripts/MainMenu/MainMenuButtons.js
+++ TankTest/Assets/Scripts/MainMenu/MainMenuButtons.js
@@ -4,10 +4,16 @@ function LoadDesertScene()
  {
      Application.LoadLevel(1);
  }
- function LoadBeachScene()
+ function LoadCityScene()
  {
      Application.LoadLevel(2);
  }
+ function LoadLeague()
+ {
+ 	Application.LoadLevel(3);
+ }
 function QuitGame() {
+     EditorApplication.isPlaying = false;
+
 	Application.Quit();
 }
