4c64af9139d5b321934c9129e70ccbbfb00ae16a
wht931011 <wht931011@gmail.com>
2017-12-12 00:25:13 -0500

    Add random pick map feature

--- TankTest/Assets/Scripts/MainMenu/MainMenuButtons.js
+++ TankTest/Assets/Scripts/MainMenu/MainMenuButtons.js
@@ -1,19 +1,24 @@
 ﻿import UnityEngine.SceneManagement;
 import UnityEngine.Debug;
-function LoadDesertScene()
- {
-     Application.LoadLevel(1);
- }
- function LoadCityScene()
- {
-     Application.LoadLevel(2);
- }
- function LoadLeague()
- {
- 	Application.LoadLevel(3);
- }
+
+function LoadDesertScene(){
+	Application.LoadLevel(1);
+}
+
+function LoadCityScene(){
+	Application.LoadLevel(2);
+}
+
+function LoadLeague(){
+	Application.LoadLevel(3);
+}
+
+function RandomMap(){
+	Application.LoadLevel(Random.Range(1,4));
+}
+
 function QuitGame() {
-     EditorApplication.isPlaying = false;
+	EditorApplication.isPlaying = false;
 
 	Application.Quit();
 }
